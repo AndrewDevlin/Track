21458deb4569acdb2a65296f9cb13d49d08afe82
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-13 16:30:09 -0800

    add .hide() to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,38 @@ $(document).ready(function() {
     var frontorback = $("#frontorback").val();
 
     if (environment === "Startup - I don't mind taking a risk on an exiting new venture.") {
+      $("#hidec").hide();
       $("#hideruby").show();
       $("#hidephp").show();
       $("#hidejava").show();
     }
-
     if (environment === "large") {
+      $("#hideruby").hide();
       $("#hidec").show();
       $("#hidephp").show();
       $("#hidejava").show();
     }
 
     if (truthvbeauty === "Logic") {
+      $("#hidedesign").hide();
+      $("#hidecss").hide();
       $("#hidec").show();
       $("#hideruby").show();
       $("#hidephp").show();
     }
-
     if (truthvbeauty === "Aesthetics") {
+      $("#hidec").hide();
+      $("#hideruby").hide();
+      $("#hidephp").hide();
       $("#hidedesign").show();
       $("#hidecss").show();
     }
+
+    if (apps === "Web") {
+      $("#hidejavascript").show();
+    }
+
+
     event.preventDefault();
   });
 });
